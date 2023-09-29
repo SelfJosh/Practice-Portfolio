a5421f8839c3820818eb1331fe002141cf65fbae
Josh Becerra <99226546+SelfJosh@users.noreply.github.com>
2023-09-29 00:17:17 -0600

    Update scripts.js

--- Potfolio v1/scripts.js
+++ Potfolio v1/scripts.js
@@ -1,3 +1,6 @@
+var customTime = 1;
+var posY;
+
 var boxes = [];
 var boxes1 = [];
 
@@ -22,45 +25,51 @@ function setup() {
     }
     
 }
-
-function draw() {
+for(i = 0; i < 20; i++){
+    setTimeout(function(){
+        customTime++;
+        posY = customTime * 30;
+    }, 6000)
+}
+function draw() 
+{
     background(255);
     translate(-1250,-500)
     push();
-    boxes.forEach(function(){
-        push();
-        rotateZ(frameCount * 0.005)
-        boxes[0].display();
-        pop();
-        translate(50,0)
-    }) 
+    boxLoop();
     pop();
-
+    translate(0,posY)
     push();
-    translate(0,30)
-    boxes.forEach(function(){
-        push();
-        rotateZ(frameCount * 0.005)
-        boxes1[0].display();
-        pop();
-        translate(50,0)
-    })
+    boxLoop();
     pop();
 }
 
 
 function myBox() {
-    this.display = function () {
-        box(20)
+    this.display = function () 
+    {
+        box(25)
     }
 }
 
-function myBox1() {
-    this.display = function () {
+function myBox1() 
+{
+    this.display = function () 
+    {
         translate(0,0)
-        box(20)
+        box(25)
     }
 }
-function animation(item, index, arr){
-    
+
+function boxLoop()
+{
+    boxes.forEach(function()
+        {
+            push();
+            rotateZ(frameCount * 0.005)
+            boxes[0].display();
+            pop();
+            translate(50,0)
+        })
 }
+
